bc60b6a0a892f15d7dcaca253847d9abe68cb376
Tomas <tywe@kth.se>
2023-04-05 16:30:14 +0200

    save token in localstorage

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import { useEffect } from 'react';
 import '../styles/login.css';
 
 
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=bacf3feb739549c986cf242ff05225b8&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
-
-function extractTokenAfterAuth(hash) {
-    const urlParams = new URLSearchParams(hash);
-    const token = urlParams.get('code');
-    return token;
-}
-
-function putToken() {
-    if(window.location.hash) {
-        const access_token = extractTokenAfterAuth(window.location.hash);
-        console.log("This is access_token");
-        console.log(access_token);
-        localStorage.clear();
-        localStorage.setItem("access_token", access_token);
-    }
-}
-
 
 export default function Login(){
     
