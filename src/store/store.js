f91a0a8e85a88e9a9801a02064f8b552163b1a6f
Tomas <tywe@kth.se>
2023-05-10 15:43:09 +0200

    add redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,26 @@ import { configureStore } from "@reduxjs/toolkit";
 import { playHistoryReducer } from "../slices/recentTracksSlice";
 import { tokenReducer } from "../slices/tokenSlice";
 import { userSpotifyPlistReducer } from "../slices/userSpotifyPlist";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage
+};
+
+const reducer = combineReducers({
+    tokeninfo : tokenReducer,
+    playedHistory : playHistoryReducer,
+    userSpotifyPlayList : userSpotifyPlistReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = configureStore({
-    reducer: {
-        tokeninfo : tokenReducer,
-        playedHistory : playHistoryReducer,
-        userSpotifyPlayList : userSpotifyPlistReducer,
-    }
+    reducer: persistedReducer
 })
 
 export default store;
