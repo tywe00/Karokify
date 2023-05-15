5d044e9ea19ce2894ddaa1f969d85c2e0eb6d77f
Rafael Bechara <75490904+raeef96@users.noreply.github.com>
2023-05-15 12:51:16 +0200

    its working , finally!!!!!!!

--- src/slices/recentTracksSlice.js
+++ src/slices/recentTracksSlice.js
@@ -9,6 +9,12 @@ const recentTracksSlice = createSlice({
     initialState,
     reducers: {
         addTrack: (state, action) => {
+            for (let i = 0; i < state.recentTracksList.length; i++) {
+                   if (state.recentTracksList[i].id === action.payload.id) {
+                    state.recentTracksList.splice(i, 1);
+                    break;
+                   }
+                 }
             const length = state.recentTracksList.unshift(action.payload);  //add track at beginning of list
             if(length > 10) {     //remove the last one in list
                 state.recentTracksList.pop();
